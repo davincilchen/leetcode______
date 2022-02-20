64fcc528c97f6f4ae7642bf9126c2b9f8c0fb033
doki <doki2099@gmail.com>
2022-02-20 23:09:17 +0800

    update

---
+++ Anagram Check by Characters Count_2/question.go
@@ -0,0 +1,12 @@
+package main
+
+//
+//https://siongui.github.io/2017/05/08/go-anagram-check-by-char-count/
+
+// Check whether two strings are anagram of each other in Go programming language.
+
+// Check by characters count:
+
+// setup map structures to count the characters of both strings [2].
+// compare if the two map structures are equal [3] via reflect.DeepEqual.
+// Another way to check anagram is by sorting, see [1].
